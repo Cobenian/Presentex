1ea04f8618590f238dbe2a6660a0ea41ba53c261
Bryan Weber <bryan@cobenian.com>
2015-05-03 21:26:36 -0400

    adding generated sample presentation

---
+++ sample/present.js
@@ -0,0 +1,30 @@
+$("body").keydown(function(e) {
+  if(e.keyCode == 37) { // left
+    console.log('left');
+    var slide = $('.slide:visible');
+    var w = "+=" +  (slide.width() * 3);
+    if ( slide.prev('.slide').length > 0 ) {
+      slide.animate({
+        left: w
+      }, function() {
+        slide.hide().css({'left': 0});
+        slide.prev('.slide').css({'left': 0}).show();
+      });
+    }
+  }
+  else if(e.keyCode == 39) { // right
+    console.log('right');
+    var slide = $('.slide:visible');
+    var w = "-=" + (slide.width() * 3);
+    if ( slide.next('.slide').length > 0 ) {
+      slide.animate({
+        left: w
+      }, function() {
+        slide.hide().css({'left': 0});
+        slide.next('.slide').css({'left': 0}).show();
+      });
+    }
+  }
+});
+
+$(".slide").first().show();
